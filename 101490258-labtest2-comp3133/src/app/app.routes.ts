a43138162babe36f99cfe1352ae4b5c7a2381ab4
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-04-08 17:37:50 -0400

    routing and navigation

--- 101490258-labtest2-comp3133/src/app/app.routes.ts
+++ 101490258-labtest2-comp3133/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Characterlist } from './components/characterlist/characterlist';
+import { Characterdetails } from './components/characterdetails/characterdetails';
+import { Characterfilter } from './components/characterfilter/characterfilter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Characterlist },
+  { path: 'character/:id', component: Characterdetails },
+  { path: 'filter', component: Characterfilter }
+];
