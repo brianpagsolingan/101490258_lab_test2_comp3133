a70e5ac3175f8580eb2325d5e90b726888ca63f6
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-04-08 15:14:49 -0400

    characterlist done, character filter component start

---
+++ 101490258-labtest2-comp3133/src/app/components/characterfilter/characterfilter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characterfilter',
+  imports: [],
+  templateUrl: './characterfilter.html',
+  styleUrl: './characterfilter.scss',
+})
+export class Characterfilter {
+
+}
