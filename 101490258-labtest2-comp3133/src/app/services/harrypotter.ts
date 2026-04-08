cb2ae1086374dbd52fa59cfb3fa83d0f3ca9c582
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-04-08 13:58:47 -0400

    configure httpclient and animations

--- 101490258-labtest2-comp3133/src/app/services/harrypotter.ts
+++ 101490258-labtest2-comp3133/src/app/services/harrypotter.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Character } from '../models/character';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Harrypotter {
-  
+  private baseUrl = 'https://hp-api.onrender.com/api'
+  constructor(private http: HttpClient) {}
+
+  getAllCHaracters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.baseUrl}/characters`);
+  }
+
+  getCharactersByHouse(house: string): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.baseUrl}/characters/house/${house}`);
+  }
+
+  getCharacterById(id: string): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.baseUrl}/characters/${id}`);
+  }
 }
